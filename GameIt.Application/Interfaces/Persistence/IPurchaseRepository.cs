f6694fd4478e55f6a74b4f6bd1d593ac31be42a0
BeRo <beroooberooo6@gmail.com>
2025-07-01 15:42:18 +0300

    Add generic repository and unit of work interfaces

---
+++ GameIt.Application/Interfaces/Persistence/IPurchaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameIt.Domain;
+
+namespace GameIt.Application.Interfaces.Persistence
+{
+    public interface IPurchaseRepository : IGenericRepository<Purchase>
+    {
+
+    }
+}
---
+++ GameIt.Application/Interfaces/Persistence/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameIt.Application.Interfaces.Persistence
+{
+    public interface IUnitOfWork
+    {
+        IGameRepository Games { get; }
+        IWishlistRepository Wishlists { get; }
+        IDiscountRepository Discounts { get; }
+        IReviewRepository Reviews { get; }
+        ICommentRepository Comments { get; }
+        ICategoryRepository Categories { get; }
+        IPurchaseRepository Purchases { get; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
