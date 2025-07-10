3df750303caa73303b4e0c8661ba76aec2919c94
BeRo <beroooberooo6@gmail.com>
2025-07-10 11:01:56 +0300

    Add Entity Framework and Database Context

---
+++ GameIt.Persistence/DatabaseContext/GameItDbContext.cs
@@ -0,0 +1,57 @@
+﻿using GameIt.Domain;
+using GameIt.Domain.Common;
+using GameIt.Domain.EntityMapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameIt.Persistence.DatabaseContext;
+
+public class GameItDbContext : DbContext
+{
+    public GameItDbContext(DbContextOptions<GameItDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Game> Games { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Comment> comments { get; set; }
+    public DbSet<Discount> discounts { get; set; }
+    public DbSet<Publisher> Publishers { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<Wishlist> Wishlists { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new GameMapping());
+        modelBuilder.ApplyConfiguration(new CategoryMapping());
+        modelBuilder.ApplyConfiguration(new CommentMapping());
+        modelBuilder.ApplyConfiguration(new DiscountMapping());
+        modelBuilder.ApplyConfiguration(new PublisherMapping());
+        modelBuilder.ApplyConfiguration(new PurchaseMapping());
+        modelBuilder.ApplyConfiguration(new ReviewMapping());
+        modelBuilder.ApplyConfiguration(new WishlistMapping());
+        modelBuilder.ApplyConfiguration(new ApplicationUserMapping());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        // Automatically set the CreatedAt and UpdatedAt properties
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+        {
+            entry.Entity.UpdatedAt = DateTime.Now;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+        }
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+
+}
--- GameIt.Persistence/PersistenceServiceRegistration.cs
+++ GameIt.Persistence/PersistenceServiceRegistration.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using GameIt.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GameIt.Persistence;
 public static class PersistenceServiceRegistration
 {
-    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
+        // Register the DbContext with the connection string from configuration
+        services.AddDbContext<GameItDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("GameItDbConnectionString")));
         return services;
     }
 }
