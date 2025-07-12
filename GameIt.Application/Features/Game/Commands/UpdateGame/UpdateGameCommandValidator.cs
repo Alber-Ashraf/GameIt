1ec7606bc9d90e018333f03e899243da8ef04227
BeRo <beroooberooo6@gmail.com>
2025-07-12 13:48:37 +0300

    Update "Create and Update" Validators

--- GameIt.Application/Features/Game/Commands/UpdateGame/UpdateGameCommandValidator.cs
+++ GameIt.Application/Features/Game/Commands/UpdateGame/UpdateGameCommandValidator.cs
@@ -23,21 +23,16 @@ public class UpdateGameCommandValidator : AbstractValidator<UpdateGameCommand>
             .MaximumLength(500).WithMessage("Game description must not exceed 500 characters.");
 
         RuleFor(x => x.ImageUrl)
-            .NotEmpty().WithMessage("Image URL is required.")
-            .Must(url => Uri.IsWellFormedUriString(url, UriKind.Absolute))
-            .WithMessage("Image URL must be a valid absolute URL.");
+            .NotEmpty().WithMessage("Image URL is required.");
 
         RuleFor(x => x.Price)
             .GreaterThanOrEqualTo(0).WithMessage("Price must be a non-negative value.");
 
         RuleFor(x => x.Size)
-            .NotEmpty().WithMessage("Game size is required.")
-            .Matches(@"^\d+(\.\d+)?\s?(MB)$").WithMessage("Size must be in MB format.");
+            .NotEmpty().WithMessage("Game size is required.");
 
         RuleFor(x => x.DownloadLink)
-            .NotEmpty().WithMessage("Download link is required.")
-            .Must(url => Uri.IsWellFormedUriString(url, UriKind.Absolute))
-            .WithMessage("Download link must be a valid absolute URL.");
+            .NotEmpty().WithMessage("Download link is required.");
 
         RuleFor(x => x.CategoryName)
             .NotEmpty().WithMessage("Category name is required.");
