9e7ac1cdbf80b393305d6559a5c9c9de5b00365e
BeRo <beroooberooo6@gmail.com>
2025-07-12 11:44:41 +0300

    Implement Get All Games List endpoint

---
+++ GameIt.API/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using GameIt.Application.Features.Game.Queries.GetAllGameDetails;
+using GameIt.Application.Features.Game.Queries.GetAllGameLists;
+using GameIt.Application.Features.Game.Queries.GetGamesByCategory;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameIt.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class GameController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public GameController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    // Get: api/game
+    [HttpGet]
+    public async Task<List<GamesListDto>> GetGames()
+    {
+        var query = new GetAllGamesListQuery();
+        var result = await _mediator.Send(query);
+
+        // If no games are found, return an empty list
+        if (result == null || !result.Any())
+        {
+            return new List<GamesListDto>();
+        }
+
+        return result;
+    }
+}
