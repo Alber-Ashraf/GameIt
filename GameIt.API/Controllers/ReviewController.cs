05da703426337bc7e0f678666b047db7d54493f1
BeRo <beroooberooo6@gmail.com>
2025-07-13 17:49:43 +0300

    Implement Review Controller with all endpoints

---
+++ GameIt.API/Controllers/ReviewController.cs
@@ -0,0 +1,61 @@
+﻿using GameIt.Application.Features.Review.Commands.CreateReview;
+using GameIt.Application.Features.Review.Commands.DeleteReview;
+using GameIt.Application.Features.Review.Commands.UpdateReview;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ReviewIt.Application.Features.Review.Queries.GetReviewDetails;
+
+namespace GameIt.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReviewController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public ReviewController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    // Get: api/review/{gameId}
+    [HttpGet("{gameId}")]
+    public async Task<IActionResult> GetReviewsByGame(
+        [FromRoute] Guid gameId,
+        CancellationToken token)
+    {
+        var query = new GetReviewsByGameQuery(gameId);
+        var reviews = await _mediator.Send(query, token);
+        return Ok(reviews);
+    }
+
+    // Post: api/review
+    [HttpPost]
+    public async Task<IActionResult> CreateReview(
+        [FromBody] CreateReviewCommand command,
+        CancellationToken token)
+    {
+        var result = await _mediator.Send(command, token);
+        return CreatedAtAction(nameof(CreateReview), new { id = result }, result);
+    }
+
+    //Update: api/review/{id}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateReview(
+        [FromRoute] Guid id,
+        [FromBody] UpdateReviewCommand command,
+        CancellationToken token)
+    {
+        var result = await _mediator.Send(command, token);
+        return NoContent();
+    }
+
+    // Delete: api/review/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReview(
+        [FromRoute] Guid id,
+        CancellationToken token)
+    {
+        var command = new DeleteReviewCommand() { Id = id};
+        await _mediator.Send(command, token);
+        return NoContent();
+    }
+}
