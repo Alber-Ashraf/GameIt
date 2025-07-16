8bfc06381c0a75421e2e1de386e679ca37d0e56d
BeRo <beroooberooo6@gmail.com>
2025-07-16 15:02:12 +0300

    Implement RemoveFromWishlist Command

---
+++ GameIt.Application/Features/Wishlist/Commands/RemoveFromWishlist/RemoveFromWishlistCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GameIt.Application.Features.Wishlist.Commands.RemoveFromWishlist;
+
+public class RemoveFromWishlistCommand : IRequest<Unit>
+{
+    public Guid GameId { get; set; }
+    public string UserId { get; set; } 
+}
---
+++ GameIt.Application/Features/Wishlist/Commands/RemoveFromWishlist/RemoveFromWishlistCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using GameIt.Application.Exeptions;
+using GameIt.Application.Interfaces.Persistence;
+using MediatR;
+
+namespace GameIt.Application.Features.Wishlist.Commands.RemoveFromWishlist;
+
+public class RemoveFromWishlistCommandHandler : IRequestHandler<RemoveFromWishlistCommand, Unit>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    public RemoveFromWishlistCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+    public async Task<Unit> Handle(RemoveFromWishlistCommand request, CancellationToken token)
+    {
+        // Validate the request
+        var validator = new RemoveFromWishlistCommandValidator(_unitOfWork);
+        var validationResult = validator.Validate(request);
+        if (!validationResult.IsValid)
+            throw new BadRequestException("Invalid Wishlist Item", validationResult);
+
+        // Check if the game is in the user's wishlist
+        var exists = await _unitOfWork.Wishlists.IsGameInWishlistAsync(request.UserId, request.GameId);
+
+        if (!exists)
+            throw new BadRequestException("This game is not in your wishlist.");
+
+        // Remove the item from the wishlist
+        await _unitOfWork.Wishlists.RemoveFromWishlistAsync(request.UserId, request.GameId, token);
+
+        // Save changes
+        await _unitOfWork.SaveChangesAsync(token);
+
+        // Return Unit to indicate success
+        return Unit.Value;
+
+    }
+}
