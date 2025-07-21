4ab08d90f9a83fe0f44f83e10634a9ac80794f02
BeRo <beroooberooo6@gmail.com>
2025-07-21 13:27:17 +0300

    Implement Unit Test for GetAllGamesList

---
+++ GameIt.Application.UnitTests/Features/Game/Queries/GetAllGamesListQueryHandlerTest.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using GameIt.Application.Features.Game.Queries.GetAllGameLists;
+using GameIt.Application.Interfaces.Persistence;
+using GameIt.Application.MappingProfiles;
+using GameIt.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+
+namespace GameIt.Application.UnitTests.Features.Game.Queries;
+
+public class GetAllGamesListQueryHandlerTest
+{
+    private IMapper _mapper;
+    private readonly Mock<IUnitOfWork> _mockGameRepository;
+    public GetAllGamesListQueryHandlerTest()
+    {
+        _mockGameRepository = MockGameRepository.GetMockGameRepository();
+
+        // Setup the mock repository to return a predefined list of games
+        var config = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new GameProfile());
+        });
+        _mapper = config.CreateMapper();
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnAllGamesList_WhenGamesExist()
+    {
+        // Arrange
+        var handler = new GetAllGamesListQueryHandler(_mapper, _mockGameRepository.Object);
+        var query = new GetAllGamesListQuery();
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+        // Shouldly assertions
+        result.ShouldBeOfType<List<GamesListDto>>();
+        result.Count.ShouldBeGreaterThan(0);
+    }
+}
---
+++ GameIt.Application.UnitTests/Mocks/MockGameRepository.cs
@@ -0,0 +1,53 @@
+﻿using GameIt.Application.Interfaces.Persistence;
+using GameIt.Domain;
+using Moq;
+
+namespace GameIt.Application.UnitTests.Mocks;
+
+public class MockGameRepository
+{
+    private static readonly List<Game> _games = new()
+    {
+        new Game
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Game 1",
+            Description = "Description 1",
+            Price = 19.99m,
+            IsFree = false,
+            IsFeatured = true,
+            CategoryId = Guid.NewGuid(),
+        },
+        new Game
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Game 2",
+            Description = "Description 2",
+            Price = 29.99m,
+            IsFree = false,
+            IsFeatured = false,
+            CategoryId = Guid.NewGuid(),
+        },
+        new Game
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test Game 3",
+            Description = "Description 3",
+            Price = 0.00m, 
+            IsFree = true,
+            IsFeatured = false,
+            CategoryId = Guid.NewGuid(),
+        }
+    };
+
+    public static Mock<IUnitOfWork> GetMockGameRepository()
+    {
+        var mockRepo = new Mock<IUnitOfWork>();
+
+        // Get All Games test
+        mockRepo.Setup(repo => repo.Games.GetAllWithCategoryAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(_games);
+
+        return mockRepo;
+    }
+}
