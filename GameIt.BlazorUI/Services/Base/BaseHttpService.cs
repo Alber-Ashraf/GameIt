37e40aa0a14a25834c44aff59fca02481cb62d9f
BeRo <beroooberooo6@gmail.com>
2025-07-22 12:28:01 +0300

    Update Blazor config using NSwagStudio for consuming GameIt Api

---
+++ GameIt.BlazorUI/Services/Base/BaseHttpService.cs
@@ -0,0 +1,10 @@
+﻿namespace GameIt.BlazorUI.Services.Base;
+
+public class BaseHttpService
+{
+    protected readonly IClient _client;
+    public BaseHttpService(IClient client)
+    {
+        _client = client;
+    }
+}
