99edee34b72ff8ffd0e1901baddedad5aa07f0a1
BeRo <beroooberooo6@gmail.com>
2025-07-23 10:22:04 +0300

    Setting up Register and Login Services

---
+++ GameIt.Application/Interfaces/Identity/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using GameIt.Application.Models.Identity;
+
+namespace GameIt.Application.Interfaces.Identity;
+
+public interface IAuthService
+{
+    Task<AuthResponse> Login(AuthRequest authRequest);
+    Task<RegistrationResponse> Register(RegistrationRequest registrationRequest);
+}
---
+++ GameIt.Application/Interfaces/Identity/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using GameIt.Application.Models.Identity;
+
+namespace GameIt.Application.Interfaces.Identity;
+
+public interface IUserService
+{
+    Task<List<Employee>> GetEmployees();
+    Task<Employee> GetEmployee(string userId);
+}
